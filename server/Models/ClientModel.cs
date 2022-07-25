a1a6fa52bbd5ee798220a0d63b245426b7e24806
Mehmet <43003253+mgcanturk@users.noreply.github.com>
2022-07-25 23:36:48 +0300

    server classes and models created

---
+++ server/Models/ClientModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace server.Models
+{
+    public class ClientModel
+    {
+        public Socket socket { get; set; }
+        public bool isConnected { get; set; }
+        public bool isMessageLimitExpired { get; set; }
+        public DateTime lastRecivedTime { get; set; }
+    }
+}
